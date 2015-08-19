6779c07b9c50798bc036ede9c6f9944c0da96706
Hampus Persson <h@hmps.se>
2015-08-19 20:07:38 +0200

    update helpers file

--- src/helpers.js
+++ src/helpers.js
@@ -1,7 +1,37 @@
-export function checkNecessaryParams(name, opts, params) {
+import gulp from 'gulp';
+
+
+/**
+ * Pipeline class
+ */
+
+var pipelineFactory = {
+    create(src) {
+        if (!src) { throw new Error('All task needs a src parameter.'); }
+
+        this.pipeline = gulp.src(src);
+
+        return this;
+    },
+    addPipe(params = {}) {
+        if ( params.if || params.hasOwnProperty('if') === false ) {
+            this.pipeline = this.pipeline.pipe(params.fn(params.opts));
+        }
+
+        return this;
+    }
+}
+
+
+function checkRequiredParams(name, opts, params) {
     params.forEach((item) => {
         if (!opts.hasOwnProperty(item)) {
-            throw new Error(`${item} is a necessary param for ${name} task`);
+            throw new Error(`${item} is a required param for ${name} task`);
         }
     });
 }
+
+export {
+    pipelineFactory,
+    checkRequiredParams
+};
