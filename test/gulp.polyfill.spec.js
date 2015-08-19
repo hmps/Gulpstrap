dee01318c0574f0a5a1fd3f99eaaaaae9c3204d0
Hampus Persson <h@hmps.se>
2015-08-19 20:11:18 +0200

    add gulp polyfill tests

---
+++ test/gulp.polyfill.spec.js
@@ -0,0 +1,11 @@
+/* global describe, it */
+/* eslint-disable no-unused-expressions */
+import {expect} from 'chai';
+// import gulp from 'gulp';
+import polyfills from '../dist/gulp-polyfill';
+
+describe('gulp polyfills', () => {
+    it('should pass', () => {
+        expect(true).to.be.true;
+    });
+});
