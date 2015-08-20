ec5aaffa5353a6a8a897ee30c7f0bbe40cf18f4a
Hampus Persson <h@hmps.se>
2015-08-20 13:05:59 +0200

    add bump task

---
+++ src/tasks/bump.js
@@ -0,0 +1,18 @@
+import { Task } from './task';
+
+import bump from 'gulp-bump';
+import args from '../args';
+
+function bumpFactory(opts = {}) {
+    opts.src = opts.src || './package.json';
+    let reqParams = [];
+
+    let ops = [
+        { fn: bump, opts: { type: args.bump } },
+        { gulpDest: opts.dest || './' }
+    ];
+
+    return new Task(opts.name || 'bump', opts, reqParams, ops);
+}
+
+export default bumpFactory;
