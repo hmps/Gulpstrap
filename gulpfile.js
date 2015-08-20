75777212a661a9f40750c518734a801c36fa3c8d
Hampus Persson <h@hmps.se>
2015-08-20 11:03:40 +0200

    update pattern for calling tasks in demo gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,20 @@
 // var requireDir = require('require-dir');
 // var gulp = require('gulp');
 
-var stylus = require('./dist/tasks/stylus');
 var less = require('./dist/tasks/less');
 var sass = require('./dist/tasks/sass');
 var eslint = require('./dist/tasks/eslint');
+var stylus = require('./dist/tasks/stylus');
 var clean = require('./dist/tasks/clean');
 var sequence = require('./dist/tasks/sequence');
 
-stylus.setupTask({src: 'test/src/*.styl', dest: 'test/dist/'});
-less.setupTask({src: 'test/src/*.less', dest: 'test/dist/'});
-sass.setupTask({src: 'test/src/*.scss', dest: 'test/dist/'});
-eslint.setupTask({src: 'test/src/*.js' });
-clean.setupTask({paths: ['test/dist']});
+less({name: 'less', src: 'test/src/*.less', dest: 'test/dist/'});
+sass({name: 'sass', src: 'test/src/*.scss', dest: 'test/dist/'});
+stylus({name: 'stylus', src: 'test/src/*.styl', dest: 'test/dist/'});
+eslint({name: 'eslint', src: 'test/src/*.js' });
+clean({name: 'clean', paths: ['test/dist']});
 
-sequence.parallell('parallell1', ['clean', 'stylus', 'eslint']);
-sequence.serial('serial1', ['clean', 'stylus', 'eslint']);
+sequence('parallell1', ['clean', 'stylus', 'eslint']);
 
 // Watch, tasks to run ?
 // Copy files to directory
