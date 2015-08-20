c36fa0333c9ee3b6687207d0f1a14d0784430d8c
Hampus Persson <h@hmps.se>
2015-08-20 13:08:10 +0200

    new examples in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -6,20 +6,28 @@ var sass = require('./dist/tasks/sass');
 var eslint = require('./dist/tasks/eslint');
 var stylus = require('./dist/tasks/stylus');
 var clean = require('./dist/tasks/clean');
+var copy = require('./dist/tasks/copy');
+var bump = require('./dist/tasks/bump');
+
 var sequence = require('./dist/tasks/sequence');
+var gulpWatch = require('./dist/tasks/watch');
 
 less({name: 'less', src: 'test/src/*.less', dest: 'test/dist/'});
 sass({name: 'sass', src: 'test/src/*.scss', dest: 'test/dist/'});
 stylus({name: 'stylus', src: 'test/src/*.styl', dest: 'test/dist/'});
 eslint({name: 'eslint', src: 'test/src/*.js' });
 clean({name: 'clean', paths: ['test/dist']});
+copy({name: 'copy', src: 'test/src/**/*.js', dest: 'test/dist'});
+bump({ name: 'bump' });
 
-sequence('parallell1', ['clean', 'stylus', 'eslint']);
+gulpWatch({ name: 'watch', watchers: [
+    { path: 'test/src/test.styl', tasks: ['stylus'] },
+    { path: 'test/src/test.js', tasks: ['eslint'] }
+]});
 
-// Watch, tasks to run ?
-// Copy files to directory
+sequence('default', ['clean', [ 'stylus', 'eslint' ], 'watch']);
+
 // Karma
 // Git tag
-// Bump version
 // Create release
 // serve
