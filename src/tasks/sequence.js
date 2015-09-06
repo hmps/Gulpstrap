fd511ad2c79edc054bd7ed35489b5e906ed33ad4
Hampus Persson <h@hmps.se>
2015-09-06 06:04:56 +0200

    add check of required params in sequence method

--- src/tasks/sequence.js
+++ src/tasks/sequence.js
@@ -1,22 +1,27 @@
+import { Task } from '../task';
+
 import gulp from 'gulp';
 import runSequence from 'run-sequence';
 
 class Sequence {
-    constructor(name, tasks) {
-        this.name = 'sequence';
+    constructor(opts = {}) {
+        Task.checkRequiredParams('Sequence', opts, ['name', 'tasks']);
 
-        if ( !Array.isArray(tasks) ) {
-            tasks = [ tasks ];
+        if ( !Array.isArray(opts.tasks) ) {
+            opts.tasks = [ opts.tasks ];
         }
 
-        gulp.task(name, () => {
-            runSequence.apply(this, tasks );
+        gulp.task(opts.name, () => {
+            runSequence.apply(this, opts.tasks );
         });
     }
 }
 
 function sequenceFactory(name, tasks) {
-    return new Sequence(name, tasks);
+    return new Sequence({
+        name,
+        tasks
+    });
 }
 
 export { sequenceFactory as sequence };
